c6086b0f45bd8bf0eeb589a24e06bc67f52f0ba0
Masayoshi  Sakakura <msakakura16@Masayoshis-MacBook-Air.local>
2021-12-17 12:21:17 -0600

    NO HEAD BULLSHIT

--- unet_main.py
+++ unet_main.py
@@ -6,23 +6,13 @@ import helper
 from unet_components import *
 from typing import List
 
-<<<<<<< HEAD
 
 
 class UNet(nn.Module):
-=======
-from typing import List
-
-
-# Full implementation of baseline unet using the component
-
-class Unet(nn.Module):
->>>>>>> df94d583b6dd3dfb760b1ce970a91de0ce0571d5
     def __init__(self, 
                  in_channels = 1, 
                  out_channels = 1, 
                  base_num_filter=32, 
-<<<<<<< HEAD
                  decoder_probe_points: List[int] = None 
                  ):
 
@@ -31,11 +21,6 @@ class Unet(nn.Module):
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.base_num_filter = base_num_filter
-=======
-                 decoder_probe_points: List[int] = None ):
-        super.__init__()
-        self.decoder_probe_points = decoder_probe_points
->>>>>>> df94d583b6dd3dfb760b1ce970a91de0ce0571d5
         
         #-------- Input -------#
         self.input_conv = blk.DoubleConv(kernel_size=3, in_channels = self.in_channels, out_channels = self.base_num_filter)
@@ -54,7 +39,6 @@ class Unet(nn.Module):
         self.decoder3 = blk.UpDecoder(kernel_size = 3, in_channels = self.base_num_filter*4, out_channels = self.base_num_filter*2)
         self.decoder4 = blk.UpDecoder(kernel_size = 3, in_channels = self.base_num_filter*2, out_channels = self.base_num_filter)
 
-<<<<<<< HEAD
         '''
         if decoder_probe_points is not None : 
             assert len(self.decoder_probe_points) == 4, f'Size of decoder probe points must be at most the number of decoder blocks'
@@ -62,12 +46,6 @@ class Unet(nn.Module):
                 assert -1 < self.decoder_probe_points[i] < 4, f'Expected decoder_probe_points at index {i} to be in the range [0, 3]'\
                 f', got {self.decoder_probe_points[i]}'
         '''
-=======
-        assert len(self.decoder_probe_points) == 4, f'Size of decoder probe points must be at most the number of decoder blocks'
-        for i in range(4):
-            assert -1 < self.decoder_probe_points[i] < 4, f'Expected decoder_probe_points at index {i} to be in the range [0, 3]'\
-            f', got {self.decoder_probe_points[i]}'
->>>>>>> df94d583b6dd3dfb760b1ce970a91de0ce0571d5
         
         # ----- Output -------#
         self.out_conv = blk.OutConv(in_channels = self.base_num_filter, out_channels = self.out_channels)
@@ -76,39 +54,22 @@ class Unet(nn.Module):
         x1 = self.input_conv(x)
 
         e1 = self.encoder1(x1)
-<<<<<<< HEAD
         e2 = self.encoder2(e1)
         e3 = self.encoder3(e2)
-=======
-        e2 = self.encoder1(e1)
-        e3 = self.encoder1(e2)
->>>>>>> df94d583b6dd3dfb760b1ce970a91de0ce0571d5
         
         bottle_neck = self.bottle_neck(e3)
 
         d1 = self.decoder1(bottle_neck, e3)
         d2 = self.decoder2(d1, e2)
         d3 = self.decoder3(d2, e1)
-<<<<<<< HEAD
         d4 = self.decoder4(d3,x1)
         
         '''
-=======
-        d4 = self.decoder4(d3)
-        
->>>>>>> df94d583b6dd3dfb760b1ce970a91de0ce0571d5
         decoder_block_outputs = [d1, d2, d3, d4]
         decoder_block_intermediate_outputs = []
         for block in self.decoder_probe_points:
             decoder_block_intermediate_outputs.append(decoder_block_outputs[block])        
-<<<<<<< HEAD
         '''
 
         y = self.out_conv(d4)
         return y#, *decoder_block_intermediate_outputs
-=======
-        
-        y = self.out_conv.forward(d4)
-        return y, *decoder_block_intermediate_outputs
-
->>>>>>> df94d583b6dd3dfb760b1ce970a91de0ce0571d5
