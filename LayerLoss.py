8223e040f8f4f99087399d10c29d754550d186f9
RoySRC <roysajeeb4@gmail.com>
2021-12-18 03:05:11 -0600

    Create LayerLoss.py

---
+++ LayerLoss.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+class LayerLoss(nn.Module):
+  def __init__(self, in_channels: List[int]):
+    super.__init__()
+    
+    
+  def forward(self, _input):
+    
+    
