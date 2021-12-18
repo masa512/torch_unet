40e939490898f9d7e68e75e6a4903144ad004f0d
RoySRC <roysajeeb4@gmail.com>
2021-12-18 03:24:38 -0600

    Update LayerLoss.py

--- LayerLoss.py
+++ LayerLoss.py
@@ -1,4 +1,5 @@
 import torch
+import torchvision
 import torch.nn as nn
 
 # what is another name for someone who identifies themnselves as interdisciplinary
@@ -8,8 +9,17 @@ class LayerLoss(nn.Module):
   def __init__(self, in_channels: List[int]):
     super.__init__()
     
+    self.conv_channels = []
+    for i, channel in enumerate(in_channels):
+      self.conv_channels.append(nn.Conv2d(channel,1,1,1))
+      self.add_module(f'layerLossConv_{i}', self.conv_channels[-1])
+     
+    mse_loss = nn.MSELoss()
     
-    
-  def forward(self, _input):
-    
-    
+  
+  def forward(self, target, *_input):
+    l = 0
+    for _in in _input:
+      t = torchvision.transforms.functional.resize(target, _in.shape[2:])
+      l = l + mse_loss(, t)
+      
