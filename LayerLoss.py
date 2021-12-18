0277a70dd63ac206de943f7ead4d7097fde86074
RoySRC <roysajeeb4@gmail.com>
2021-12-18 03:08:41 -0600

    Update LayerLoss.py

--- LayerLoss.py
+++ LayerLoss.py
@@ -1,9 +1,13 @@
 import torch
 import torch.nn as nn
 
+# what is another name for someone who identifies themnselves as interdisciplinary
+# - jack of all trades, masters of none, aka michael.
+
 class LayerLoss(nn.Module):
   def __init__(self, in_channels: List[int]):
     super.__init__()
+    
     
     
   def forward(self, _input):
