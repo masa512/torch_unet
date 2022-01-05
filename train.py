b21271e198a1a096ec29afd3de1f637dd10e4277
masa512 <ms4@illinois.edu>
2022-01-05 11:11:02 -0600

    training script with proper plot and prog bar

--- train.py
+++ train.py
@@ -15,12 +15,14 @@ from torch_percloss import Perceptual_loss
 from tifffile.tifffile import imsave
 import torch.nn.functional as F
 from layer_loss import LayerLoss
+from tqdm import tqdm
+import time 
 
 
 def train_unet(network,
                 device, 
                 num_epochs: int = 1,
-                batch_size: int = 1, 
+                batch_size: int = 2, 
                 accum_step: int = 50, 
                 learning_rate = 1E-4,
                 r_train = 0.8,
@@ -30,6 +32,12 @@ def train_unet(network,
 
     # Step 1 : Load dataset and load model to device
     network.to(device=device)
+    # Save location 
+    project_name = "Focus"
+    dir_name = project_name + time.strftime("%Y%m%d-%H%M%S")
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+
 
     # Define transforms to apply in dataset
     trans = transforms.Compose([
@@ -38,22 +46,15 @@ def train_unet(network,
                                 transforms.ToTensor(),
                                ])
 
-    #train_dataset = MNIST(root=r"MNIST_train",train=True, download=True,transform=trans)
-    #test_dataset = MNIST(root=r"MNIST_test",train=False, download=True,transform=trans)
-
     # Path to the dataset
     path_img = r"/home/qli/Desktop/Masa/Hela_data"
-    dataset = helper.unet_dataset(dir = path_img, xmin = -np.pi , xmax=np.pi, ymin = -np.pi , ymax=np.pi,transform=trans)
+    dataset = helper.unet_dataset(dir = path_img, xmin = -np.pi , xmax=np.pi, ymin = -np.pi , ymax=np.pi, transform=trans)
 
     # Step 2 : Split training/val
-    print(dataset)
-    #train_set, val_set = random_split(dataset, [dataset.__len__()*r_train, dataset.__len__()*(1-r_train)], generator=torch.Generator().manual_seed(0))
     train_count = round(r_train*dataset.__len__())
     val_count = dataset.__len__()-train_count
-
     train_set, val_set = random_split(dataset, [train_count,val_count])
-    #train_set = train_dataset
-    #val_set  = train_dataset
+
     # Step 3 : Dataloader in order to shuffle dateset in batches for training efficiency
     trainloader = DataLoader(train_set, shuffle=True, batch_size=batch_size)
     valloader = DataLoader(val_set, shuffle=False, drop_last=True,batch_size=batch_size)
@@ -67,85 +68,82 @@ def train_unet(network,
     #  Define a vector for validation loss
     train_loss = []
     val_loss = []
-    
-    # initialize zero vector of loss (len(loss)=num_epochs)
-    
     num_batches = train_set.__len__()//batch_size
 
-    # Define three loss functions : Perceptual, pixel-wise loss, layer-wise loss
-    
-    if Perceptual_loss:
-        criterion = percloss().to(device=device)
-    if pix_loss:
-        criterion = torch.nn.MSELoss()
-        
-    if layer_loss:
-#             loss += helper.layer_combined_loss(network = network,gt_batch = gtmid, pred_batch = ymid)
-        criterion = LayerLoss(3, device='cuda') # 2 intermediate and 1 for output
-    
+    # Make loss function as dictionary
+    losses = {
+        "perceptual loss" : percloss().to(device=device),
+        "mse" : torch.nn.MSELoss(),
+        "layer loss" : LayerLoss(3, device='cuda'),
+        "ssim" : helper.SSIM_loss(),
+        "pearson" : helper.pearson_loss()
+    }
+
+
+    # Training Begins
+    best_val_loss = 1000 # anything high to begin with 
     for t in range(num_epochs):
         print(f"-------------------EPOCH {t}-----------------------")
         
-        for i, batch in enumerate(trainloader):# Extract one permutation of training data on the GPU
+        epoch_loss = []
+        ti = time.time()
+        for i, batch in enumerate(tqdm(trainloader)):# Extract one permutation of training data on the GPU
             network.train() # Training mode
-            #input_batch = helper.AddGaussNoise(0,0.1)(batch[0].to(device=device, dtype=torch.float32))
             input_batch = batch['Input'].to(device=device, dtype=torch.float32)
             gt_batch = batch['GT'].to(device=device, dtype=torch.float32)
-            y, intermediate = network(input_batch)# Prediction output with layer
+            y, intermediate = network(input_batch)# Prediction output with layer tuple
             
+            loss = 0
             if layer_loss:
-                loss = criterion(gt_batch, y, *intermediate)
-            
+                loss += losses['layer loss'](gt_batch, y, *intermediate) 
             if pix_loss:
-                loss = criterion(gt_batch,y)/accum_step
-            
+                loss += losses['mse'](gt_batch,y)
+                loss += losses['ssim'](gt_batch,y)
+                loss += losses['pearson'](gt_batch,y)
             if Perceptual_loss:
-                loss = criterion(yhat=y,y=gt_batch,blocks=[0, 0, 1, 0])
-
+                loss += losses['perceptual loss'](yhat=y,y=gt_batch,blocks=[0, 0, 1, 0])
             
-            # Backprop
-#             network.eval() # evaluation mode
-
-            loss.backward()
+            loss.backward() # Accumulate gradient
+            epoch_loss.append(loss.item()) # Add current batch loss
             # If multiple of accum step -> update the parameters and zero_grad 
             if (i+1) % accum_step == 0 or i+1 == len(trainloader):
-                train_loss.append(loss.item())
                 optimizer.step()
                 optimizer.zero_grad()
-                print(f"----> epoch{t}, iter{i}: Training loss = {loss}")
-            
+    
+        network.eval() # Change to evaluation mode when evaluating validation loss
         with torch.no_grad():
             # Evaluate epoch loss every end of epoch
             val_l = 0
             for j, batch_val in enumerate(valloader):# Extract one permutation of training data on the GPU
-            #input_batch = helper.AddGaussNoise(0,0.1)(batch[0].to(device=device, dtype=torch.float32))
                 input_batch_val = batch_val['Input'].to(device=device, dtype=torch.float32)
                 gt_batch_val = batch_val['GT'].to(device=device, dtype=torch.float32)
-                pred_batch_val = network(input_batch_val)# Prediction output
-                # Define three loss functions : Perceptual, pixel-wise loss, layer-wise loss
-                '''
-                if Perceptual_loss:
-                    criterion = Perceptual_loss().to(device=device)
-                    loss = criterion(yhat=pred_batch,y=gt_batch,blocks=[0, 0, 1, 0])
-                '''
-                
+                pred_batch_val = network(input_batch_val)# Prediction output              
                 loss_val = F.mse_loss(gt_batch_val, pred_batch_val[0])
                 val_l += loss_val
-            val_loss.append(val_l/valloader.__len__())          
-            
+            val_loss.append(val_l/valloader.__len__())    
+        deltat = time.time()-ti
+        train_loss.append(sum(epoch_loss)/len(epoch_loss))    
+        print(f'===> Epoch {t}: Train Loss -> {train_loss[-1]}')
+        print(f'===> Epoch {t}: Validation Loss -> {val_loss[-1]}')
+        print(f"====> epoch{t}: Time elapsed = {deltat//60}mins")
 
-            network.train()
-            
+        if (t+1)%5 == 0 and t < num_epochs-1 and val_loss[-1] < best_val_loss: # If best validation loss so far -> Save the model
+            torch.save(network.state_dict(), os.path.join(dir_name,f'model_epoch{t+1}.pt'))
+            best_val_loss = val_loss[-1] # Update the best validation loss 
+
+
+        network.train()
+
         # Could be nice if we could save model every few epochs
 
     # Step 7 : Save model
-    torch.save(network.state_dict(), 'final_model.pt')
+    torch.save(network.state_dict(), os.path.join(dir_name,'final_model.pt'))
 
     #================================== Step 8 : Save validation outputs accordingly ==================================#
     network.eval()
 
     # Path to save images 
-    out_path = 'val_outputs'
+    out_path = os.path.join(dir_name,'val_outputs')
     if not os.path.exists(out_path):
         os.makedirs(out_path)
     in_path = os.path.join(out_path,'in_img')
@@ -158,70 +156,45 @@ def train_unet(network,
     if not os.path.exists(pred_path):
         os.makedirs(pred_path)
 
-    test_loss = []
+    #test_loss = []
     with torch.no_grad():
         for i, batch in enumerate(valloader):
             input_batch = batch['Input'].to(device=device, dtype=torch.float32)
             gt_batch = batch['GT'].to(device=device, dtype=torch.float32)
             pred_batch = network(input_batch)# Prediction output
-            '''
-            if Perceptual_loss:
-                    criterion = Perceptual_loss().to(device=device)
-                    loss = criterion(yhat=pred_batch,y=gt_batch,blocks=[0, 0, 1, 0])
-            '''
-            if pix_loss:
-                criterion = torch.nn.MSELoss()
-                loss = criterion(gt_batch,pred_batch)
-            '''
-            
-            if layer_loss:
-                loss += helper.layer_combined_loss(network = network,gt_batch = gt_batch,pred_batch = pred_batch)
-            '''
-            test_loss.append(loss)
             # Save image
             imsave(os.path.join(in_path,f'image{i}.tif'),input_batch.cpu().detach().numpy())
             imsave(os.path.join(gt_path,f'image{i}.tif'),gt_batch.cpu().detach().numpy())
             imsave(os.path.join(pred_path,f'image{i}.tif'),pred_batch[0].cpu().detach().numpy())
 
+    # Save plots
     plt.plot(train_loss)
+    plt.savefig(os.path.join(dir_name,'train_loss.png'))
+    plt.close()
     plt.plot(val_loss)
-    plt.legend(["train","val"])
-    plt.savefig('val_loss.png')
-        
+    plt.savefig(os.path.join(dir_name,'val_loss.png'))
+    plt.close()
+
         
 
-
+# MAIN FUNCTION
 if __name__ == '__main__':
-    '''
-    # Define the file path for input/output pair
-    _filepath = ''
-    indir = os.path.join(_filepath,'inputs')
-    gtdir = os.path.join(_filepath,'gt')
-    dir_checkpoint = os.path.join(_filepath,'check_points')
-    '''
-    '''
-    network = Unet( 
-                 in_channels = 1, 
-                 out_channels = 1, 
-                 base_num_filter=32, 
-                 decoder_probe_points = None 
-                 )
-    '''
+
     network = UNet(decoder_probe_points = [1,3])
 
     if torch.cuda.is_available():
         print(f"The CUDA GPU IS USED with msg {torch.cuda.is_available()}")
     else:
-        print("Not really working: Running with CPU")
+        print("GPU not really working: Running with CPU")
     train_unet(network=network, 
                 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu'), 
-                num_epochs = 1,
-                batch_size = 1,
-                learning_rate = 1E-4,
+                num_epochs = 30,
+                batch_size = 2,
+                learning_rate = 4E-4,
                 r_train = 0.8,
-                Perceptual_loss=True,
+                Perceptual_loss=False,
                 pix_loss = False,
-                layer_loss=False
+                layer_loss=True
                 )
 
     
